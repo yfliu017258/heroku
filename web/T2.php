8784484c83745cdc04840bb79c41d0a3641a33b7
cies96035 <cies9001005@gmail.com>
2019-10-20 04:18:06 +0800

    bt

--- web/T2.php
+++
@@ -1,9 +0,0 @@
-<?php
-header(“Content-type: image/png”);
-$im = @imagecreatetruecolor(50, 100)
-or die(“Cannot Initialize new GD image stream”);
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5, “A Simple Text String”, $text_color);
-imagepng($im);
-imagedestroy($im);
-?>
