cdc0634ded86251eb37531f66629bf1421f79808
cies96035 <cies9001005@gmail.com>
2019-10-21 22:29:25 +0800

    test

--- web/index.php
+++ web/index.php
@@ -23,3 +23,53 @@ $app->get('/', function() use($app) {
 });
 
 $app->run();
+
+	$page = $requestInfo->{'page'};
+	if (!is_null($page)) {
+		handleComments($page['id'], $siteInfo);
+		if (isset($_POST["wb_form_id"])) handleForms($page['id'], $siteInfo);
+	}
+	ob_start();
+	if ($page) {
+		$fl = dirname(__FILE__).'/'.$page['file'];
+		if (is_file($fl)) {
+			ob_start();
+			include $fl;
+			$out = ob_get_clean();
+			$ga_out = '';
+			if ($lang && $langs) {
+				foreach ($langs as $ln => $default) {
+					$pageUri = getPageUri($page['id'], $ln, $siteInfo);
+					$out = str_replace(urlencode('{{lang_'.$ln.'}}'), $pageUri, $out);
+				}
+			}
+			if (is_file($ga_tpl = dirname(__FILE__).'/ga.php')) {
+				ob_start(); include $ga_tpl; $ga_out = ob_get_clean();
+			}
+			$out = str_replace('<ga-code/>', $ga_out, $out);
+			$baseUrl = (isHttps() ? 'https' : 'http').'://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost').'/';
+			$out = str_replace('{{base_url}}', $baseUrl, $out);
+			$out = str_replace('{{curr_url}}', $baseUrl.($lang && $lang != $def_lang ? $lang.'/' : '').$route, $out);
+			$out = str_replace('{{hr_out}}', $hr_out, $out);
+			header('Content-type: text/html; charset=utf-8', true);
+			echo $out;
+		}
+	} else {
+		header("Content-type: text/html; charset=utf-8", true, 404);
+		if (is_file(dirname(__FILE__).'/404.html')) {
+			include '404.html';
+		} else {
+			echo "<!DOCTYPE html>\n";
+			echo "<html>\n";
+			echo "<head>\n";
+			echo "<title>404 Not found</title>\n";
+			echo "</head>\n";
+			echo "<body>\n";
+			echo "404 Not found\n";
+			echo "</body>\n";
+			echo "</html>";
+		}
+	}
+	ob_end_flush();
+
+?>
