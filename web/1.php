85ae1a148aee2d47be77031e255df25ed2703180
cies96035 <cies9001005@gmail.com>
2019-10-22 22:15:21 +0800

    Revert "v20191022"
    This reverts commit 75fa4f50b05d055e165a5f8077cfb22a82e7e03c.

--- web/1.php
+++ web/1.php
@@ -1,3 +1,3 @@
 <title>隨機gif</title>
 <body bgcolor="	#444444">
-</span> <a href="https://cies96035.herokuapp.com/imgrandom.php#png" imageanchor="1" style="clear: left; float: left; margin-bottom: 1em; margin-right: 1em; text-align: center;"><span style="clear: left; float: left; font-size: x-small; margin-bottom: 1em; margin-right: 1em;"><img border="0" data-original-height="300" data-original-width="300" height="320" src="https://cies96035.herokuapp.com/imgrandom.php#png" width="320" /></span></a>
+</span> <a href="https://cies96035.herokuapp.com/exam.png" imageanchor="1" style="clear: left; float: left; margin-bottom: 1em; margin-right: 1em; text-align: center;"><span style="clear: left; float: left; font-size: x-small; margin-bottom: 1em; margin-right: 1em;"><img border="0" data-original-height="300" data-original-width="300" height="320" src="https://cies96035.herokuapp.com/exam.png" width="320" /></span></a>
