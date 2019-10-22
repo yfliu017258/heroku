e6937516338de8750303fa8bbd39fdc2a15e38be
cies96035 <cies9001005@gmail.com>
2019-10-22 23:39:29 +0800

    v20191022

--- web/index.php
+++ web/index.php
@@ -1,49 +1,24 @@
 <?php
 
 require('../vendor/autoload.php');
-	$page = $requestInfo->{'page'};
-	
-	ob_start();
-	if ($page) {
-		$fl = dirname(__FILE__).'/'.$page['file'];
-		if (is_file($fl)) {
-			ob_start();
-			include $fl;
-			$out = ob_get_clean();
-			$ga_out = '';
-			if ($lang && $langs) {
-				foreach ($langs as $ln => $default) {
-					$pageUri = getPageUri($page['id'], $ln, $siteInfo);
-					$out = str_replace(urlencode('{{lang_'.$ln.'}}'), $pageUri, $out);
-				}
-			}
-			if (is_file($ga_tpl = dirname(__FILE__).'/ga.php')) {
-				ob_start(); include $ga_tpl; $ga_out = ob_get_clean();
-			}
-			$out = str_replace('<ga-code/>', $ga_out, $out);
-			$baseUrl = (isHttps() ? 'https' : 'http').'://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost').'/';
-			$out = str_replace('{{base_url}}', $baseUrl, $out);
-			$out = str_replace('{{curr_url}}', $baseUrl.($lang && $lang != $def_lang ? $lang.'/' : '').$route, $out);
-			$out = str_replace('{{hr_out}}', $hr_out, $out);
-			header('Content-type: text/html; charset=utf-8', true);
-			echo $out;
-		}
-	} else {
-		header("Content-type: text/html; charset=utf-8", true, 404);
-		if (is_file(dirname(__FILE__).'/404.html')) {
-			include '404.html';
-		} else {
-			echo "<!DOCTYPE html>\n";
-			echo "<html>\n";
-			echo "<head>\n";
-			echo "<title>404 Not found</title>\n";
-			echo "</head>\n";
-			echo "<body>\n";
-			echo "404 Not found\n";
-			echo "</body>\n";
-			echo "</html>";
-		}
-	}
-	ob_end_flush();
 
-?>
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
+$app->run();
