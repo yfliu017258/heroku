035281218c772a192039b88f725fac752e0fae8b
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-06 22:46:31 +0800

    Update 12

--- web/test.php
+++ web/test.php
@@ -3,9 +3,9 @@ header( 'Content-type: image/svg+xml' );
 
 
 $R = 290;
-$SL = 280;
-$ML = 250;
-$HL = 200;
+$SL = 250;
+$ML = 200;
+$HL = 150;
 $backgroundX = 600;
 $backgroundY = 600;
 $panelX = 300;
@@ -22,9 +22,9 @@ echo <<<SVG
 	<circle id="panel" cx="{$panelX}" cy="{$panelY}" r="{$R}" fill="none" stroke="#000000" stroke-width="10"/>
 	<circle id="point" cx="{$panelX}" cy="{$panelY}" r="10" fill="#000000" stroke="#000000" stroke-width="10"/>
 	
-	<rect id="second"  x="{$panelX}" y="{$panelY}" width="10" height="{$SL}" fill="#000000" stroke="#000000" stroke-width="10" stroke-linecap="round"/>
-	<rect id="minute"  x="{$panelX}" y="{$panelY}" width="20" height="{$ML}" fill="#333333" stroke="#333333" stroke-width="10" stroke-linecap="round"/>
-	<rect id="hour"  x="{$panelX}" y="{$panelY}" width="50" height="{$HL}" fill="#666666" stroke="#666666" stroke-width="10" stroke-linecap="round"/>
+	<polyline id="second"  points="{$panelX},{$panelY} {$panelX},{$panelY}+{$SL}" fill="#000000" stroke="#000000" stroke-width="2" stroke-linecap="round"/>
+	<polyline id="minute"  points="{$panelX},{$panelY} {$panelX},{$panelY}+{$ML}" fill="#333333" stroke="#333333" stroke-width="5" stroke-linecap="round"/>
+	<polyline id="hour"  points="{$panelX},{$panelY} {$panelX},{$panelY}+{$HL}" fill="#666666" stroke="#666666" stroke-width="15" stroke-linecap="round"/>
 </svg>
 SVG;
 ?> 
