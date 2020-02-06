6604451145d0e9a2c111f77273faed2dd6c220ec
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-06 22:35:26 +0800

    Update 11

--- web/test.php
+++ web/test.php
@@ -3,6 +3,9 @@ header( 'Content-type: image/svg+xml' );
 
 
 $R = 290;
+$SL = 280;
+$ML = 250;
+$HL = 200;
 $backgroundX = 600;
 $backgroundY = 600;
 $panelX = 300;
@@ -19,7 +22,9 @@ echo <<<SVG
 	<circle id="panel" cx="{$panelX}" cy="{$panelY}" r="{$R}" fill="none" stroke="#000000" stroke-width="10"/>
 	<circle id="point" cx="{$panelX}" cy="{$panelY}" r="10" fill="#000000" stroke="#000000" stroke-width="10"/>
 	
-	<rect id="second" >
+	<rect id="second"  x="{$panelX}" y="{$panelY}" width="10" height="SL" fill="#000000" stroke="#000000" stroke-width="10" stroke-linecap="round"/>
+	<rect id="minute"  x="{$panelX}" y="{$panelY}" width="20" height="ML" fill="#333333" stroke="#333333" stroke-width="10" stroke-linecap="round"/>
+	<rect id="hour"  x="{$panelX}" y="{$panelY}" width="50" height="HL" fill="#666666" stroke="#666666" stroke-width="10" stroke-linecap="round"/>
 </svg>
 SVG;
 ?> 
