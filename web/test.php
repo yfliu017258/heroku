7264021bb85600fbc4564ebf8ce6c27e5d548bef
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-06 22:25:37 +0800

    Update 10

--- web/test.php
+++ web/test.php
@@ -10,17 +10,16 @@ $panelY = 300;
 
 
 echo <<<SVG
-	<?xml version="1.0" standalone="no"?>
-	
-	<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+<?xml version="1.0" standalone="no"?>	
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
 	"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> 
-	
-	<svg width="{$backgroundX}" height="{$backgroundY}" version="1.1"
+<svg width="{$backgroundX}" height="{$backgroundY}" version="1.1"
 	 xmlns="http://www.w3.org/2000/svg"> 
 	 
-	<circle cx="{$panelX}" cy="{$panelY}" r="{$R}" fill="none" stroke="#000000" stroke-width="10"/>
-	<circle cx="{$panelX}" cy="{$panelY}" r="10" fill="#000000" stroke="#000000" stroke-width="10"/>
+	<circle id="panel" cx="{$panelX}" cy="{$panelY}" r="{$R}" fill="none" stroke="#000000" stroke-width="10"/>
+	<circle id="point" cx="{$panelX}" cy="{$panelY}" r="10" fill="#000000" stroke="#000000" stroke-width="10"/>
 	
+	<rect id="second" >
 </svg>
 SVG;
 ?> 
