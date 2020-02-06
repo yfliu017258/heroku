11596955300953a67e370eefc05ca54fdcd9ccf8
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-06 21:23:42 +0800

    Update 2

--- web/test.php
+++ web/test.php
@@ -9,13 +9,13 @@ $panelY = 300
 
 
 echo <<<SVG
-	<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+	<?xml version="1.0" standalone="no"?>
 	<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
 	"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> 
 	<svg width="{$backgroundX}" height="{$backgroundY}" version="1.1" xmlns="http://www.w3.org/2000/svg">
 	 
-	<circle fill="none" cx="{$panelX}" cy="{$panelX}" r="{$R}" stroke="#000000" stroke-width="10" stroke-linejoin="round">
+	//<circle fill="none" cx="{$panelX}" cy="{$panelX}" r="{$R}" stroke="#000000" stroke-width="10" stroke-linejoin="round">
 	 
 	</svg>
-	SVG;
+SVG;
 ?> 
