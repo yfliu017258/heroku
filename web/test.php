f9b90a117614faac77a39b446ddf1e8ff1ffefec
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-06 21:26:48 +0800

    Update 3

--- web/test.php
+++ web/test.php
@@ -12,7 +12,11 @@ echo <<<SVG
 	<?xml version="1.0" standalone="no"?>
 	<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
 	"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> 
-	<svg width="{$backgroundX}" height="{$backgroundY}" version="1.1" xmlns="http://www.w3.org/2000/svg">
+	//<svg width="{$backgroundX}" height="{$backgroundY}" version="1.1" xmlns="http://www.w3.org/2000/svg">
+	<svg width="{$viewportX}" height="{$viewportY}" version="1.1"
+	 viewBox="0,0,{$bdgboxX},{$bdgboxY}"
+	 preserveAspectRatio="xMidYMid meet"
+	 xmlns="http://www.w3.org/2000/svg"> 
 	 
 	//<circle fill="none" cx="{$panelX}" cy="{$panelX}" r="{$R}" stroke="#000000" stroke-width="10" stroke-linejoin="round">
 	 
