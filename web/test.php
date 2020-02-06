73df2411193c5afffdc87f603cd1826a818687db
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-06 23:35:23 +0800

    16

--- web/test.php
+++ web/test.php
@@ -22,6 +22,21 @@ echo <<<SVG
 	<circle id="panel" cx="{$PX}" cy="{$PY}" r="{$R}" fill="none" stroke="#000000" stroke-width="10"/>
 	<circle id="point" cx="{$PX}" cy="{$PY}" r="10" fill="#000000" stroke="#000000" stroke-width="10"/>
 	
+	<line id="second"  x1="{$PX}" y1="{$PY}" x2="{$PX}" y2="100" fill="#000000" stroke="#000000" stroke-width="2" stroke-linecap="round"> 
+		<animateTransform dur="60s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
+	</line>
+	<line id="minute"  x1="{$PX}" y1="{$PY}" x2="{$PX}" y2="100" fill="#333333" stroke="#333333" stroke-width="5" stroke-linecap="round">
+		<animateTransform dur="60s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
+	</line>
+	<line id="hour"  x1="{$PX}" y1="{$PY}" x2="{$PX}" y2="100" fill="#666666" stroke="#666666" stroke-width="15" stroke-linecap="round">
+		<animateTransform dur="60s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$panelY}"/>
+	</line>
+	
+	//20 560/6 
+	<text x="393,487,580,487,393,300,207,113,20" y="113,207,300,393,487,580,487,393,300" style="font-size:24px;">123456</text>  
+	//<text x="0,20,50,80,120" y="60,80,70,55,60" style="font-size:24px;">10</text>  
+	//<text x="0,20,50,80,120" y="60,80,70,55,60" style="font-size:24px;">11</text>  
+	//<text x="0,20,50,80,120" y="60,80,70,55,60" style="font-size:24px;">12</text>  
 </svg>
 SVG;
 ?> 
