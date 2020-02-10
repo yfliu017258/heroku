fef42e09fe500f6644c169cd87fed9e77245507c
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-11 02:15:22 +0800

    update

--- web/clock.php
+++ web/clock.php
@@ -34,37 +34,37 @@ echo <<<SVG
 	
 	<defs>
 		<radialGradient id="a1" cx="20%" cy="20%">
-	    	<stop offset="5%" stop-color="#eff" />
-	    	<stop offset="95%" stop-color="#6ff" />
+	    	<stop offset="5%" stop-color="#cff" />
+	    	<stop offset="95%" stop-color="#133570" />
 		</radialGradient>
 	</defs>
 	
 	<circle id="panel" cx="{$PX}" cy="{$PY}" r="{$R}" fill="url(#a1)" stroke="#BBFFEE" stroke-width="10"/>
 	
-	<line id="hour"  x1="{$PX}" y1="{$PY}" x2="{$HX}" y2="{$HY}" fill="#133570" stroke="#133570" stroke-width="20" stroke-linecap="round">
+	<line id="hour"  x1="{$PX}" y1="{$PY}" x2="{$HX}" y2="{$HY}" fill="#66ffff" stroke="#66ffff" stroke-width="20" stroke-linecap="round">
 		<animateTransform dur="43200s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
-	<line id="minute"  x1="{$PX}" y1="{$PY}" x2="{$MX}" y2="{$MY}" fill="#133570" stroke="#133570" stroke-width="7" stroke-linecap="round">
+	<line id="minute"  x1="{$PX}" y1="{$PY}" x2="{$MX}" y2="{$MY}" fill="#66ffff" stroke="#66ffff" stroke-width="7" stroke-linecap="round">
 		<animateTransform dur="3600s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
-	<line id="second"  x1="{$PX}" y1="{$PY}" x2="{$SX}" y2="{$SY}" fill="#133570" stroke="#133570" stroke-width="2" stroke-linecap="round"> 
+	<line id="second"  x1="{$PX}" y1="{$PY}" x2="{$SX}" y2="{$SY}" fill="#66ffff" stroke="#66ffff" stroke-width="2" stroke-linecap="round"> 
 		<animateTransform dur="60s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
 	
 	<circle id="point" cx="{$PX}" cy="{$PY}" r="10" fill="#BBFFEE" stroke="#BBFFEE" stroke-width="5"/>
 	
-	<text x="430" y="75" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">I</text>  
-	<text x="525" y="170" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">II</text>  
-	<text x="560" y="300" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">III</text>  
-	<text x="525" y="430" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">IV</text>  
-	<text x="430" y="525" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">V</text>  
-	<text x="300" y="560" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">VI</text>  
-	<text x="170" y="525" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">VII</text>  
-	<text x="85" y="430" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">VIII</text>  
-	<text x="40" y="300" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">IX</text>  
-	<text x="75" y="170" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">X</text>  
-	<text x="170" y="75" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">XI</text>  
-	<text x="300" y="40" text-anchor="middle" alignment-baseline="central" style="font-size:55px;">XII</text>  
+	<text x="430" y="75" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">I</text>  
+	<text x="525" y="170" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">II</text>  
+	<text x="560" y="300" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">III</text>  
+	<text x="525" y="430" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">IV</text>  
+	<text x="430" y="525" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">V</text>  
+	<text x="300" y="560" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">VI</text>  
+	<text x="170" y="525" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">VII</text>  
+	<text x="85" y="430" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">VIII</text>  
+	<text x="40" y="300" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">IX</text>  
+	<text x="75" y="170" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">X</text>  
+	<text x="170" y="75" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">XI</text>  
+	<text x="300" y="40" text-anchor="middle" alignment-baseline="central" style="font-size:55px;" fill="#eff">XII</text>  
 </svg>
 SVG;
 ?> 
