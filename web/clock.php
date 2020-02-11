27b3d29177a8feb2a9fd98906fa83848b442f37c
cies96035 <39860649+cies96035@users.noreply.github.com>
2020-02-11 14:42:28 +0800

    最終更新

--- web/clock.php
+++ web/clock.php
@@ -17,11 +17,11 @@ $bgdY = 600;
 $PX = 300;
 $PY = 300;
 
-$HX=sin(deg2rad($H*30))*$R*0.4+$PX;
-$MX=sin(deg2rad($M*6))*$R*0.65+$PX;
+$HX=sin(deg2rad($H*30))*$R*0.35+$PX;
+$MX=sin(deg2rad($M*6))*$R*0.6+$PX;
 $SX=sin(deg2rad($S*6))*$R*0.8+$PX;
-$HY=sin(deg2rad($H*30-90))*$R*0.4+$PY;
-$MY=sin(deg2rad($M*6-90))*$R*0.65+$PY;
+$HY=sin(deg2rad($H*30-90))*$R*0.35+$PY;
+$MY=sin(deg2rad($M*6-90))*$R*0.6+$PY;
 $SY=sin(deg2rad($S*6-90))*$R*0.8+$PY;
 
 
@@ -58,10 +58,10 @@ echo <<<SVG
 	<line id="hour"  x1="{$PX}" y1="{$PY}" x2="{$HX}" y2="{$HY}" stroke="#6ff" stroke-width="15" stroke-linecap="round" stroke-opacity="0.5">
 		<animateTransform dur="43200s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
-	<line id="minute"  x1="{$PX}" y1="{$PY}" x2="{$MX}" y2="{$MY}" stroke="#6ff" stroke-width="7" stroke-linecap="round" stroke-opacity="0.7">
+	<line id="minute"  x1="{$PX}" y1="{$PY}" x2="{$MX}" y2="{$MY}" stroke="#6ff" stroke-width="7.5" stroke-linecap="round" stroke-opacity="0.75">
 		<animateTransform dur="3600s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
-	<line id="second"  x1="{$PX}" y1="{$PY}" x2="{$SX}" y2="{$SY}" stroke="#6ff" stroke-width="2" stroke-linecap="round"> 
+	<line id="second"  x1="{$PX}" y1="{$PY}" x2="{$SX}" y2="{$SY}" stroke="#6ff" stroke-width="2.5" stroke-linecap="round"> 
 		<animateTransform dur="60s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
 	
