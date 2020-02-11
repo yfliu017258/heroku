6d1f397109a5d1fd1e10777fd0ce6ef2ea86116a
cies96035 <cies9001005@gmail.com>
2020-02-11 15:45:21 +0800

    Update test.php

--- web/test.php
+++ web/test.php
@@ -54,7 +54,7 @@ echo <<<SVG
 	</defs>
 	
 	<circle id="panel" cx="{$PX}" cy="{$PY}" r="{$R}" fill="url(#a1)" stroke="url(#a3)" stroke-width="10"/>
-	/*
+	
 	<line id="hour"  x1="{$PX}" y1="{$PY}" x2="{$HX}" y2="{$HY}" stroke="#6ff" stroke-width="15" stroke-linecap="round" stroke-opacity="0.5">
 		<animateTransform dur="43200s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
 	</line>
@@ -63,11 +63,11 @@ echo <<<SVG
 	</line>
 	<line id="second"  x1="{$PX}" y1="{$PY}" x2="{$SX}" y2="{$SY}" stroke="#6ff" stroke-width="2.5" stroke-linecap="round"> 
 		<animateTransform dur="60s" attributeName="transform" repeatCount="indefinite" type="rotate" from="0,{$PX},{$PY}" to="360,{$PX},{$PY}"/>
-	</line>*/
+	</line>
 	
-	<image href="https://cies96035.herokuapp.com/img/CL/H.png" height="{$HL}" width="300" x="{$PX}" y="{$PY}/>
-	<image href="https://cies96035.herokuapp.com/img/CL/M.png" height="{$ML}" width="300" x="{$PX}" y="{$PY}/>
-	<image href="https://cies96035.herokuapp.com/img/CL/S.png" height="{$SL}" width="300" x="{$PX}" y="{$PY}/>
+	<image href="https://cies96035.herokuapp.com/img/CL/H.png" height="{$HL}" width="300" x="{$PX}" y="{$PY}"/>
+	<image href="https://cies96035.herokuapp.com/img/CL/M.png" height="{$ML}" width="300" x="{$PX}" y="{$PY}"/>
+	<image href="https://cies96035.herokuapp.com/img/CL/S.png" height="{$SL}" width="300" x="{$PX}" y="{$PY}"/>
 
 	
 	<circle id="point" cx="{$PX}" cy="{$PY}" r="10" fill="#050520" stroke="url(#a3)" stroke-width="5"/>
