c9eacde7f7dfaefc583371b25816e23b60f2c920
cies96035 <cies9001005@gmail.com>
2020-06-19 03:03:02 +0800

    MainPage Change

--- web/1.php
+++
@@ -1,3 +0,0 @@
-<title>隨機gif</title>
-<body bgcolor="	#444444">
-</span> <a href="https://cies96035.herokuapp.com/images/exam.php#.png" imageanchor="1" style="clear: left; float: left; margin-bottom: 1em; margin-right: 1em; text-align: center;"><span style="clear: left; float: left; font-size: x-small; margin-bottom: 1em; margin-right: 1em;"><img border="0" data-original-height="300" data-original-width="300" height="320" src="https://cies96035.herokuapp.com/images/exam.php#.png" width="320" /></span></a>
